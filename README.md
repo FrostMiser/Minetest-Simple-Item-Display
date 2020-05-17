dd73be8b2bc4ff539ae8aa61f318910d087d5bad
FrostMiser <FrostMiser@users.noreply.github.com>
2020-05-17 15:00:12 -0500

    Rework to only display item name

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# WiTT
-What is This Thing, a Minetest mod that functions similarly to the WAILA mod for Minecraft.
-Currently there are two ways to use this mod: The server way and the client way. 
-The server way is what you should do when playing singleplayer, just take the master branch and put it in your mods/ directory.
-The client way is what you should do when playing on a server that does not have WiTT installed. Take the `csm` branch and put it in your clientmods/ directory, then enable it.
-If you are using the client way, you will need to use the command `.witt`
-
-A video of the mod at version 1 (server mode, pre-initial commit) is available [here](https://youtu.be/dfIfj8nrCQg).
+# Minetest Simple Item Display
+Mintest Simple Item Display is fork of WiTT(What is This Thing). This is a Minetest mod that displays the name of the node you are looking at when you are near it.
--- init.lua
+++ init.lua
@@ -1,12 +1,9 @@
 local player_to_id_text = {} -- Storage of players so the mod knows what huds to update
-local player_to_id_mtext = {}
-local player_to_id_image = {}
 local player_to_cnode = {} -- Get the current looked at node
-local player_to_animtime = {} -- For animation
-local player_to_animon = {} -- For disabling animation
-local player_to_enabled = {} -- For disabling WiTT
+local player_to_enabled = {} -- Enable/disable item display
 
-local ypos = 0.1
+local ypos = 0.9
+local xpos = 0.45
 
 minetest.register_globalstep(function(dtime) -- This will run every tick, so around 20 times/second
     for _, player in ipairs(minetest:get_connected_players()) do -- Do everything below for each player in-game
@@ -14,20 +11,12 @@ minetest.register_globalstep(function(dtime) -- This will run every tick, so aro
         if not player_to_enabled[player] then return end -- Don't do anything if they have it disabled
         local lookat = get_looking_node(player) -- Get the node they're looking at
 
-        player_to_animtime[player] = math.min((player_to_animtime[player] or 0.4) + dtime, 0.5) -- Animation calculation
-
-        if player_to_animon[player] then -- If they have animation on, display it
-            update_player_hud_pos(player, player_to_animtime[player])
-        end
 
         if lookat then 
             if player_to_cnode[player] ~= lookat.name then -- Only do anything if they are looking at a different type of block than before
-                player_to_animtime[player] = nil -- Reset the animation
-                local nodename, mod = describe_node(lookat) -- Get the details of the block in a nice looking way
+                local nodename = describe_node(lookat) -- Get the details of the block in a nice looking way
                 player:hud_change(player_to_id_text[player], "text", nodename) -- If they are looking at something, display that
-                player:hud_change(player_to_id_mtext[player], "text", mod)
                 local node_object = minetest.registered_nodes[lookat.name] -- Get information about the block
-                player:hud_change(player_to_id_image[player], "text", handle_tiles(node_object)) -- Pass it to handle_tiles which will return a texture of that block (or nothing if it can't create it)
             end
             player_to_cnode[player] = lookat.name -- Update the current node
         else
@@ -44,39 +33,14 @@ minetest.register_on_joinplayer(function(player) -- Add the hud to all players
         text = "test",
         number = 0xffffff,
         alignment = {x = 1, y = 0},
-        position = {x = 0.5, y = ypos},
-    })
-    player_to_id_mtext[player] = player:hud_add({ -- Add the mod name text
-        hud_elem_type = "text",
-        text = "test",
-        number = 0x2d62b7,
-        alignment = {x = 1, y = 0},
-        position = {x = 0.5, y = ypos+0.015},
-    })
-    player_to_id_image[player] = player:hud_add({ -- Add the block image
-        hud_elem_type = "image",
-        text = "",
-        scale = {x = 1, y = 1},
-        alignment = 0,
-        position = {x = 0.5, y = ypos},        
-        offset = {x = -40, y = 0}
+        position = {x = xpos, y = ypos},
     })
 end)
 
-minetest.register_chatcommand("wanim", { -- Command to turn witt animations on/off
-	params = "<on/off>",
-	description = "Turn WiTT animations on/off",
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		if not player then return false end
-        player_to_animon[player] = param == "on"
-        return true
-	end
-})
 
-minetest.register_chatcommand("witt", { -- Command to turn witt on/off
+minetest.register_chatcommand("item-display", { -- Command to turn item display on or off
 	params = "<on/off>",
-	description = "Turn WiTT on/off",
+	description = "Turn Item Display on or off",
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 		if not player then return false end
@@ -109,63 +73,27 @@ function get_looking_node(player) -- Return the node the given player is looking
     return lookat
 end
 
-function describe_node(node) -- Return a string that describes the node and mod
-    local mod, nodename = minetest.registered_nodes[node.name].mod_origin, minetest.registered_nodes[node.name].description -- Get basic (not pretty) info
+
+function describe_node(node) 
+    local nodename = minetest.registered_nodes[node.name].description 
     if nodename == "" then -- If it doesn't have a proper name, just use the technical one
         nodename = node.name
     end
-    mod = remove_unneeded(capitalize(mod)) -- Make it look good
-    nodename = remove_unneeded(capitalize(nodename))
-    return nodename, mod
+    nodename = capitalize(nodename):gsub("[_-]", " ") 
+    return nodename
 end
 
-function remove_unneeded(str) -- Remove characters like '-' and '_' to make the string look better
-    return str:gsub("[_-]", " ")
-end
 
 function capitalize(str) -- Capitalize every word in a string, looks good for node names
     return string.gsub(" "..str, "%W%l", string.upper):sub(2)
 end
 
-function handle_tiles(node) -- Return an image of the tile
-    local tiles = node.tiles
-    local resize_string = "^[resize:64x64"
-
-    if tiles then -- Make sure every tile is a string
-        for i,v in pairs(tiles) do
-            if type(v) == "table" then
-                if tiles[i].name then
-                    tiles[i] = tiles[i].name
-                else
-                    return ""
-                end
-            end
-        end
-
-        -- These are the types it can draw correctly
-        if node.drawtype == "normal" or node.drawtype == "allfaces" or node.drawtype == "allfaces_optional" or node.drawtype == "glasslike" or node.drawtype == "glasslike_framed" or node.drawtype == "glasslike_framed_optional" then
-            if #tiles == 1 then -- This type of block has only 1 image, so it must be on all faces
-                return minetest.inventorycube(tiles[1], tiles[1], tiles[1]) .. resize_string
-            elseif #tiles == 3 then -- This type of block has 3 images, so it's probably 1 on top, 1 on bottom, the rest on the side
-                return minetest.inventorycube(tiles[1], tiles[3], tiles[3]) .. resize_string
-            elseif #tiles == 6 then -- This one has 6 images, so display the ones we can
-                return minetest.inventorycube(tiles[1], tiles[6], tiles[5]) .. resize_string -- Not actually sure if 5 is the correct number but it's basically the same thing most of the time
-            end
-        end
-    end
-
-    return "" -- If it can't do anything, return with a blank image
-end
 
 function update_player_hud_pos(player, to_x, to_y) -- Change position of hud elements
     to_y = to_y or ypos
     player:hud_change(player_to_id_text[player], "position", {x = to_x, y = to_y})
-    player:hud_change(player_to_id_image[player], "position", {x = to_x, y = to_y})
-    player:hud_change(player_to_id_mtext[player], "position", {x = to_x, y = to_y+0.015})
 end
 
 function blank_player_hud(player) -- Make hud appear blank
     player:hud_change(player_to_id_text[player], "text", "")
-    player:hud_change(player_to_id_mtext[player], "text", "")
-    player:hud_change(player_to_id_image[player], "text", "")
 end
